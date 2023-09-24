24dda139e246361ae58034e8f6aa7fae1eca9b1f
iamkartiks <kartikeysinha40@gmail.com>
2023-09-24 17:23:32 +0530

    commit

--- wait-for-db.sh
+++ wait-for-db.sh
@@ -1,16 +1,8 @@
 #!/bin/sh
 
-# wait-for-db.sh
-set -e
-
-host="$db"
-shift
-cmd="$@"
-
-until mysqladmin ping -h "$host" --silent; do
-  >&2 echo "MySQL is unavailable - sleeping"
-  sleep 1
+while ! nc -z db 3306 ; do
+    echo "Waiting for the MySQL Server"
+    sleep 3
 done
 
->&2 echo "MySQL is up - executing command"
-exec $cmd
+python manage.py runserver 0.0.0.0:8000
