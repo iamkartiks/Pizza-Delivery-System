1b301d7a86c8f77ad296aa7a90507a2ca8fd2cfe
iamkartiks <kartikeysinha40@gmail.com>
2023-10-28 15:05:59 +0530

    final-commit

--- wait-for-db.sh
+++ wait-for-db.sh
@@ -2,7 +2,7 @@
 
 while ! nc -z db 3306 ; do
     echo "Waiting for the MySQL Server"
-    sleep 3
+    sleep 5
 done
 
-python manage.py runserver 0.0.0.0:8000
+# python manage.py runserver 0.0.0.0:8000
